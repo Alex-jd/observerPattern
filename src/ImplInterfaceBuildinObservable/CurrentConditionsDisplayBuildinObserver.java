d5f435d6df57fa9f9d9299ef306ffe9e9765b59b
alex_jd <twoalexan@mail.ru>
2017-04-19 12:19:38 +0500

    implement with Buildin class Observable and interface Observer.

---
+++ src/ImplInterfaceBuildinObservable/CurrentConditionsDisplayBuildinObserver.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package ImplInterfaceBuildinObservable;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import Service.DisplayElement;
+
+/**
+ * @author alex_jd
+ *
+ */
+public class CurrentConditionsDisplayBuildinObserver implements Observer, DisplayElement{
+	
+	Observable observable;
+	private float temperature;
+	private float humidity;
+	
+	public CurrentConditionsDisplayBuildinObserver (Observable observable) {
+		this.observable = observable;
+		observable.addObserver(this);
+	}
+	
+	@Override
+	public void display() {
+		System.out.println("Buildin...Current conditions: " + temperature + "F degrees and " + humidity + "% humidity");
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		if (o instanceof WeatherDataObservable) {
+			WeatherDataObservable weatherDataObservable = (WeatherDataObservable) o;
+			this.temperature = weatherDataObservable.getTemperature();
+			this.humidity = weatherDataObservable.getHumidity();
+			display();
+		}
+	}
+
+}
---
+++ src/Test/WeatherStationBildinObservable.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package Test;
+
+import ImplInterface.CurrentConditionsDisplay;
+import ImplInterface.WeatherData;
+import ImplInterfaceBuildinObservable.CurrentConditionsDisplayBuildinObserver;
+import ImplInterfaceBuildinObservable.WeatherDataObservable;
+
+/**
+ * @author alex_jd
+ *
+ */
+public class WeatherStationBildinObservable {
+	
+public static void main(String[] args) {
+		
+	WeatherDataObservable weatherDataObservable = new WeatherDataObservable();
+		
+	CurrentConditionsDisplayBuildinObserver currentConditionsDisplayBuildinObserver = 
+			new CurrentConditionsDisplayBuildinObserver(weatherDataObservable);
+		
+	weatherDataObservable.setMeasurements(80, 65, 30.4f);
+	weatherDataObservable.setMeasurements(82, 70, 29.2f);
+	weatherDataObservable.setMeasurements(78, 90, 29.2f);
+
+	}
+
+}
